e1e7b58f779d8a96bc27195e376470ed03704a50
Mohammad Abdussalam <mohammadahmad01722@gmail.com>
2022-07-16 15:00:53 +0200

    Updated the ZIP Code data to only download Chicago

--- get_data.sh
+++ get_data.sh
@@ -10,7 +10,7 @@ echo "Downloading the datasets..."
 echo "---------------------------"
 
 wget -nc https://star.cs.ucr.edu/datasets/Chicago%20Crimes/download.json.gz -O data/chicagoCrimes/Chicago_Crimes.json.gz
-wget -nc https://star.cs.ucr.edu/datasets/TIGER2018/ZCTA5/download.json.gz -O data/zcta5/TIGER2018_ZCTA5.json.gz
+wget -nc https://star.cs.ucr.edu/datasets/TIGER2018/ZCTA5/download.json.gz?mbr=-88.07241,41.62385,-87.14613,42.05043 -O data/zcta5/TIGER2018_ZCTA5.json.gz
 
 
 echo ""; echo ""
